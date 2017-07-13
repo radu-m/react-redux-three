436ad5dd85327945c38a2fd8582b99dc9613a0cc
bigotin <radu.miu@gmail.com>
2017-07-13 19:04:31 +0200

    Simple cube running

--- spacetr/src/js/components/App.js
+++ spacetr/src/js/components/App.js
@@ -1,14 +1,41 @@
-var React = require('react');
-var TodoList = require('./TodoList');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import React3 from 'react-three-renderer';
+import * as THREE from 'three';
+// var THREE = React3.THREE;
 
-var App = React.createClass({
-  render: function() {
+class Simple extends React.Component {
+  constructor(props, context) {
+    super(props, context);
+
+    this.cameraPosition = new THREE.Vector3(0, 1.911, 5);
+  }
+
+  render() {
+    const width = window.innerWidth;
+    const height = window.innerHeight;
+
     return (
-      <div>
-        <TodoList />
-      </div>
+      <React3 mainCamera="camera" width={width} height={height}>
+        <scene>
+          <perspectiveCamera
+            name="camera"
+            fov={75}
+            aspect={width / height}
+            near={0.1}
+            far={1000}
+            position={this.cameraPosition}
+          />
+          <mesh>
+            <boxGeometry width={1} height={1} depth={1} />
+            <meshBasicMaterial color={0x00ff00} />
+          </mesh>
+        </scene>
+      </React3>
     );
-  },
-});
+  }
+}
 
-module.exports = App;
+ReactDOM.render(<Simple />, document.getElementById('root'));
+
+export default Simple;
